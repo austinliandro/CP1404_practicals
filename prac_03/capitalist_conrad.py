a612d7e11e8d2113b22ee418cc615824885ceec0
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-07-27 12:52:56 +0800

    add feature number_of_days

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,14 +14,17 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
+FILENAME = "hello.txt"
+
+out_file = open(FILENAME, 'w')
 
 price = INITIAL_PRICE
 number_of_days = 0
-price_change = 0
-print(f"Starting price: ${price:,.2f}")
 
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
+    price_change = 0
     number_of_days += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
@@ -35,4 +38,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
