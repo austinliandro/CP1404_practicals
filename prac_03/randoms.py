6be55bc2b242e574cf34e2f03d4a7b99328855ba
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-07-27 12:36:41 +0800

    string formatting

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.randint(5, 20))  # line 1 : 5 smallest, 20 largest
+print(random.randrange(3, 10, 2))  # line 2 : 3 smallest, 9 largest
+print(random.uniform(2.5, 5.5))  # line 3 : 2.5 smallest, 5.5 largest
