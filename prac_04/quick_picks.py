5ca4783753271770ddc912ff313b676d7f9cc909
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-08-06 20:20:57 +0800

    quick pick program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,26 +6,35 @@ Quick pick program
 import random
 
 NUMBERS_PER_LINE = 6
-MINIMUM = 1
-MAXIMUM = 45
+MAX_NUMBER = 45
+MIN_NUMBER = 1
 
 
 def main():
-    """Quick picks program - choose sets of random numbers."""
-    number_of_quick_picks = int(input("How many quick picks? "))
-    while number_of_quick_picks < 0:
-        print("That makes no sense!")
-        number_of_quick_picks = int(input("How many quick picks? "))
+    number_quick_pick = get_valid_quick_pick_number()
+    picking_number(number_quick_pick)
 
-    for i in range(number_of_quick_picks):
-        quick_pick = []
-        for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
-            while number in quick_pick:
-                number = random.randint(MINIMUM, MAXIMUM)
-            quick_pick.append(number)
-        quick_pick.sort()
-        print(" ".join(f"{number:2}" for number in quick_pick))
+
+def get_valid_quick_pick_number():
+    """ Get the valid  number of quick pick number """
+    number_quick_pick = int(input("How many quick picks? "))
+    while number_quick_pick < 0:
+        print("Number of quick pick can not less than 0")
+        number_quick_pick = int(input("How many quick picks? "))
+    return number_quick_pick
+
+
+def picking_number(number_quick_pick):
+    """ Processing the picking number """
+    for line in range(number_quick_pick):
+        rand_numbers = []
+        for rand_number in range(NUMBERS_PER_LINE):
+            rand_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            while rand_number in rand_numbers:
+                rand_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            rand_numbers.append(rand_number)
+        rand_numbers.sort()
+        print(" ".join(f"{rand_number:2}" for rand_number in rand_numbers))
 
 
 main()
