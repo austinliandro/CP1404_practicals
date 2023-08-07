8609b1a82956f60d3ba46598e8963798290c8f79
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-08-07 15:06:29 +0800

    formating the files

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,3 +1,9 @@
+"""
+CP1404 Practical - Austin Liandro
+State names in a dictionary
+File needs reformatting
+"""
+
 CODE_TO_NAME = {
     "QLD": "Queensland",
     "NSW": "New South Wales",
@@ -7,7 +13,16 @@ CODE_TO_NAME = {
     "VIC": "Victoria",
     "TAS": "Tasmania",
 }
+
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
+
 # to print all the code_to_name neatly
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3s} is {name}")
-
