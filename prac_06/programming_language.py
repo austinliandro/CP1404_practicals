db6bcc186863df280cb46965f73b284c210d04fd
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-08-27 23:12:23 +0800

    completed a code for ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,42 @@
+"""
+CP1404 Practical - Austin Liandro
+Programming Language class with tests.
+"""
+
+
+class ProgrammingLanguage:
+    """It wil represent the information about the programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """To construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """To return the string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """It will determine if the language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+
+def run_tests():
+    """To run a simple demo on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
