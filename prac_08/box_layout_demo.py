c9afa03679378e48da538352fdd3db0488048c6f
Austin Liandro <austin.liandro@my.jcu.edu.au>
2023-09-04 14:45:45 +0800

    updating the clear name text

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,8 +10,11 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print("test")
-        self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
 
 
 BoxLayoutDemo().run()
